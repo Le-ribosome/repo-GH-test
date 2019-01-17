152036afd4e0bba53d8c542f4573c30372f9e37c
Alexis <drouinalexis@hotmail.fr>
2019-01-17 12:59:40 +0100

    premier commit sur gitHub

---
+++ test-html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="divTest">
+      <p>Je suis un fichier php</p>
+    </div>
+  </body>
+</html>
